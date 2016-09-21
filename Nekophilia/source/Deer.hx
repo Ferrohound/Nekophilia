2261efbe29afe4b1f733252f7c2d43c77a62b5ec
Ferrohound <ferrohound@gmail.com>
2016-09-21 15:53:07 -0400

    LETS UNDO THAT ONE

--- Nekophilia/source/Deer.hx
+++ Nekophilia/source/Deer.hx
@@ -106,11 +106,6 @@ package;
 			acceleration.x = 0;
 			velocity.x = 0;
 		}
-		if (getMidpoint().distanceTo(_player2.getMidpoint()) < (_radius / 3)){
-			if(!PlayState._shadows.hasLightPoint(_player2.getMidpoint())){
-				_player2.kill();
-				}
-			}
 		super.update(elapsed);
 	 }
  }
