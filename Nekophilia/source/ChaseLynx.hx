90ecf233e268f55d575b3367a357b246fc941e6b
Ferrohound <ferrohound@gmail.com>
2016-09-21 12:51:19 -0400

    Fixing level

--- Nekophilia/source/ChaseLynx.hx
+++ Nekophilia/source/ChaseLynx.hx
@@ -14,22 +14,32 @@ package;
  class ChaseLynx extends Deer
  {
 	 public var _active:Bool = false;
+	 var _activated:Bool = false;
 	// public var _player1:Player1;
 	 //public var _player2:Player2;
 	 public function new(?X:Float=0, ?Y:Float=0){
 		 super(X, Y);
+		 _spookStem = SoundStore._stemE;
+		 _defaultStem = SoundStore._stemF;
 	 }
 	 
 	  override public function update(elapsed:Float): Void
 	 {
 		if (getMidpoint().distanceTo(_player1.getMidpoint()) < _radius && getMidpoint().distanceTo(_player2.getMidpoint()) < _radius){
-			if (_player1.x > x && _player2.x > x)
+			if (_player1.x > x && _player2.x > x){
 				_active = true;
-			else{
-				_active = false;
+				_activated = true;
+				SoundStore._stemB.fadeOut(3);
 			}
 		}
-		if (_active)
+		else{
+				_active = false;
+		}
+		if (!_active && _activated){
+			SoundStore._stemF.fadeOut(3);
+			SoundStore._stemB.fadeIn(3);
+		}
+		else if (_active)
 			super.update(elapsed);
 			
 	}
